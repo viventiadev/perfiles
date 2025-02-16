0cbbd229116920be4425f48d71f3197ff9fe119b
Rafael Ortiz Zableh <57945332+rafaelortizzableh@users.noreply.github.com>
2025-02-17 00:47:34 +0100

    feat: add english support and update form request

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,4 +1,22 @@
-function alertaPuntosTotales() {
+const translations = {
+    'es': {
+        'name': 'No olvides llenar el campo de nombre o seudónimo',
+        'ref': 'No olvides llenar el campo de referencia con el número que te dieron en la sesión',
+        'points': 'Revisa tus respuestas. Recuerda que en ninguna fila puedes repetir los valores 1,2,3 y 4'
+    },
+    'en': {
+        'name': 'Don\'t forget to fill the name or pseudonym field',
+        'ref': 'Don\'t forget to fill the reference field with the number you were given in the session',
+        'points': 'Check your answers. Remember that in no row can you repeat the values 1,2,3 and 4'
+    },
+    'cat': {
+        'name': 'No oblidis omplir el camp de nom o pseudònim',
+        'ref': 'No oblidis omplir el camp de referència amb el número que et van donar a la sessió',
+        'points': 'Revisa les teves respostes. Recorda que en cap fila pots repetir els valors 1,2,3 i 4'
+    }
+}
+
+function totalPointsAlert(language) {
     const todosLosPuntos = [];
 
     for (let index = 1; index < 73; index++) {
@@ -18,32 +36,35 @@ function alertaPuntosTotales() {
         return true;
     }
     else {
-        alert("Revisa tus respuestas. Recuerda que en ninguna fila puedes repetir los valores 1,2,3 y 4");
+        const label = translations[language].points;
+        alert(label);
         return false;
     }
 }
 
-function nombreForm() {
+function formNameAlert(language) {
     const name = document.getElementById("nombre").value;
 
     if (name == "") {
-        alert("No olvides llenar el campo de nombre o seudónimo");
+        const label = translations[language].name;
+        alert(label);
         return false;
     }
 }
 
-function referenciaForm() {
+function formReferenceAlert(language) {
     const ref = document.getElementById("referencia").value;
 
     if (ref == "") {
-        alert("No olvides llenar el campo de referencia con el número que te dieron en la sesión");
+        const label = translations[language].ref;
+        alert(label);
         return false;
     }
 }
 
-function validar() {
-    const puntos = alertaPuntosTotales();
-    const nombre = nombreForm();
-    const refer = referenciaForm();
+function validate(language) {
+    const puntos = totalPointsAlert(language);
+    const nombre = formNameAlert(language);
+    const refer = formReferenceAlert(language);
     return puntos && nombre && refer;
 }
